498388dc05ab1f54b6d6726e96731029bef5af57
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-17 10:35:55 -0300

    implementando directiva de validação de número

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,4 +1,7 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostListener
+} from '@angular/core';
 
 @Directive({
   selector: '[numero]'
@@ -6,5 +9,19 @@ import { Directive } from '@angular/core';
 export class NumeroDirective {
 
   constructor() { }
+
+  @HostListener('keyup', ['$event'])
+  onKeUp ($event: any) {
+    let valor = $event.target.value
+    let posDecimais = valor.indexOf('.')
+
+    valor = valor.replace(/[\D]/g, ''); // remover tudo que não for número de 0-9
+
+    if (posDecimais > 0) {
+      valor = valor.substr(0, posDecimais) + '.' + valor.substr(posDecimais)
+    }
+
+    $event.target.value = valor
+  }
 
 }
