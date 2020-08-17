0f5a768b6f5ba0ef4ef40246d3d9791f18ed6c23
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-17 10:55:40 -0300

    implementando acesso da diretiva com o ngModel do componente

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,14 +1,27 @@
 import {
   Directive,
-  HostListener
+  HostListener,
+  ElementRef
 } from '@angular/core';
 
-@Directive({
-  selector: '[numero]'
-})
-export class NumeroDirective {
+import {
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor
+} from '@angular/forms'
 
-  constructor() { }
+@Directive({
+  selector: '[numero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
+})
+export class NumeroDirective implements ControlValueAccessor {
+  onTouched: any;
+  onChange: any;
+
+  constructor(private el: ElementRef) { }
 
   @HostListener('keyup', ['$event'])
   onKeUp ($event: any) {
@@ -22,6 +35,21 @@ export class NumeroDirective {
     }
 
     $event.target.value = valor
+    this.onChange(valor)
+  }
+
+  registerOnChange (fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched (fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue (value: any): void {
+    this.el.nativeElement.value = value;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
   }
 
 }
